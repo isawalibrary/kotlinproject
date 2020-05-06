a2eb29979167227eb27f6940ab8be966edc3a562
isawalibrary <48917706+isawalibrary@users.noreply.github.com>
2020-05-06 16:18:39 +0100

    Add files via upload

--- Character.kt
+++ Character.kt
@@ -1,37 +1,30 @@
 class Character {
     var name: String? = null
-    var clan: Clan = Clan("String","String",0)
-    var family: Family = Family(listOf("String"),listOf("String"),0,0,"String")
+    var clan: Clan? = null
+    var family: Family? = null
     var clanring: String? = null
 }
 
-data class Clan (
-    val clanring : String? = null,
-    val clanskill : String? = null,
-    val clanstatus : Int = 0
-)
-
-class Clans {
-    companion object {
-        val Crab = Clan("Earth", "Fitness", 30)
-        val Crane = Clan("Air","Culture",35)
-    }
-
+enum class Clan (
+        val clanname : String? = null,
+        val clanring : String? = null,
+        val clanskill : String? = null,
+        val clanstatus : Int = 0
+) {
+        Crab ("Crab","Earth", "Fitness", 30),
+        Crane ("Crane","Air","Culture",35)
 }
 
-data class Family(
+enum class Family(
         val familyrings: List<String>? = (null),
         val familyskills: List<String>? = (null),
         val glory: Int = 0,
         val wealth: Int = 0,
         val clan: String? = null
-)
-
-class Families {
-    companion object {
-        val Hida = Family(listOf("Earth", "Fire"), listOf("Command", "Tactics"), 44, 4, "Crab")
-        val Hiruma = Family(listOf("Air", "Water"), listOf("Skulduggery", "Survival"), 39, 3, "Crab")
-        val Asahina = Family(listOf("Water", "Void"), listOf("Aesthetics", "Theology"), 40, 6, "Crane")
-        val Daidoji = Family(listOf("Earth", "Water"), listOf("Fitness", "Tactics"), 40, 7, "Crane")
-    }
+){
+        Hida (listOf("Earth", "Fire"), listOf("Command", "Tactics"), 44, 4, "Crab"),
+        Hiruma (listOf("Air", "Water"), listOf("Skulduggery", "Survival"), 39, 3, "Crab"),
+        Asahina (listOf("Water", "Void"), listOf("Aesthetics", "Theology"), 40, 6, "Crane"),
+        Daidoji (listOf("Earth", "Water"), listOf("Fitness", "Tactics"), 40, 7, "Crane")
 }
+
